4052c19922898e70966287026a3cf80237bbba96
gs0dv <gs0dv@main.ru>
2023-05-29 20:11:31 +0300

    added files' phone.py and test_phone.py

---
+++ src/phone.py
@@ -0,0 +1,32 @@
+from src.item import Item
+
+
+class Phone(Item):
+    """Класс для представления телефонов"""
+
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int) -> None:
+        # Метод базового класса
+        super().__init__(name, price, quantity)
+        # Количество поддерживаемых сим
+        self.__number_of_sim = number_of_sim
+
+    @property
+    def number_of_sim(self):
+        """Возвращает количество поддерживаемых сим"""
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, count_sim):
+        """Устанавливает количество поддерживаемых сим, если их число больше нуля, иначе вызов исключения"""
+        if count_sim <= 0:
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+
+        self.__number_of_sim = count_sim
+
+    def __str__(self):
+        """'iPhone 14'"""
+        return self.name
+
+    def __repr__(self):
+        """Phone('iPhone 14', 120000, 5, 2)"""
+        return f"{__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
---
+++ tests/test_phone.py
@@ -0,0 +1,31 @@
+"""Тесты с использованием pytest для класса phone.py"""
+import pytest
+
+from src.phone import Phone
+
+
+@pytest.fixture()
+def phone_1():
+    return Phone("iPhone 14", 120_000, 5, 2)
+
+
+def test_number_of_sim(phone_1):
+    assert phone_1.number_of_sim == 2
+
+
+def test_set_number_of_sim(phone_1):
+    phone_1.number_of_sim = 4
+    assert phone_1.number_of_sim == 4
+
+
+def test_set_number_of_sim__ValueError(phone_1):
+    with pytest.raises(ValueError):
+        phone_1.number_of_sim = 0
+
+
+def test_str(phone_1):
+    assert str(phone_1) == 'iPhone 14'
+
+
+def test_repr(phone_1):
+    assert repr(phone_1) == "Phone('iPhone 14', 120000, 5, 2)"
